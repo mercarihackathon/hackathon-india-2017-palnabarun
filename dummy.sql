b3366e19bcc4ca925e2486026eb36bf16515f204
Nabarun Pal <pal.nabarun95@gmail.com>
2017-10-08 13:11:03 +0530

    Adds DB sample data

---
+++ dummy.sql
@@ -0,0 +1,22 @@
+insert into agents values(1, 'A', '19.07002391', '72.88337392');
+insert into agents values(2, 'B', '19.07897897', '72.8829397');
+insert into agents values(3, 'C', '19.07273224', '72.87037636');
+insert into agents values(4, 'D', '19.08208958', '72.87725231');
+insert into agents values(5, 'E', '19.07744126', '72.87122787');
+insert into agents values(6, 'F', '19.08130888', '72.88341942');
+insert into agents values(7, 'G', '19.08278133', '72.88120458');
+insert into agents values(8, 'H', '19.07802032', '72.86954474');
+insert into agents values(9, 'I', '19.08275661', '72.87239275');
+insert into agents values(10, 'J', '19.07732067', '72.87297752');
+
+
+insert into sensors values(1, 'SA', '19.07135398', '72.87703699', 'created', 12, false);
+insert into sensors values(2, 'SB', '19.07377557', '72.87227154', 'created', 45, false);
+insert into sensors values(3, 'SC', '19.07184308', '72.87148735', 'created', 50, false);
+insert into sensors values(4, 'SD', '19.07602604', '72.8778219', 'created', 18, false);
+insert into sensors values(5, 'SE', '19.08035488', '72.87344518', 'created', 23, false);
+insert into sensors values(6, 'SF', '19.07339827', '72.87298073', 'created', 67, false);
+insert into sensors values(7, 'SG', '19.07307372', '72.87917716', 'created', 41, false);
+insert into sensors values(8, 'SH', '19.08367525', '72.87695757', 'created', 36, false);
+insert into sensors values(9, 'SI', '19.08021929', '72.88554077', 'created', 72, false);
+insert into sensors values(10, 'SJ', '19.08249497', '72.87443749', 'created', 9, false);
